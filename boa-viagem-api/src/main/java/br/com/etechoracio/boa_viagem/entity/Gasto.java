16f75a31f8175ff293969f9d2a41c6a2c551ece7
Levi Rennan <levi.costa2@etec.sp.gov.br>
2022-03-14 08:56:38 -0300

    Mapeamento JPA da classe Gasto

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
@@ -2,19 +2,48 @@ package br.com.etechoracio.boa_viagem.entity;
 
 import java.time.LocalDate;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
+@Table(name="TBL_GASTO")
+
 public class Gasto {
+	
+ @Id
+ @Column(name="ID_GASTO")
+ @GeneratedValue(strategy = GenerationType.IDENTITY)
  private Long id;
+ 
+ @Column(name="TX_DESCRICAO")
  private String descricao;
+ 
+ @Column(name="TX_LOCAL")
  private String local;
+ 
+ @Column(name="TP_CATEGORIA")
  private String categoria;
+ 
+ @Column(name="DT_GASTO")
  private LocalDate gasto;
 
+ @Column(name="VLR_GASTO")
  private Double valor;
+ 
+
+ @ManyToOne
+ @JoinColumn(name="ID_VIAGEM")
  private Viagem viagem;
  
  
