6ee66bf41317fcf066e9a0fb4882adccb06c724f
Levi Rennan <levi.costa2@etec.sp.gov.br>
2022-03-14 08:06:17 -0300

    Commit inicial

---
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
@@ -0,0 +1,17 @@
+package br.com.etechoracio.boa_viagem.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Gasto {
+ private Long id;
+ private String descricao;
+ private String local;
+ private String categoria;
+ private LocalDate gasto;
+
+ private Double valor;
+ private Viagem viagem;
+}
