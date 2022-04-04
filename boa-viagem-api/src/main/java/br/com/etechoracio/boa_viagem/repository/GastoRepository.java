849e0db95c9bcd78bac1063dd80ead07663e468f
GabrielPw <gxaavier352@gmail.com>
2022-04-04 08:51:04 -0300

    Criação do pacote controller

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/repository/GastoRepository.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/repository/GastoRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import br.com.etechoracio.boa_viagem.entity.Gasto;
 
 public interface GastoRepository extends JpaRepository<Gasto, Long>{
-
+	
+	
 	
 }
