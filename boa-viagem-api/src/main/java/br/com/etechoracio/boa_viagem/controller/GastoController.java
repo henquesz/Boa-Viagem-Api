d3a10bd17e6d61e2c06781dcdb96a0207e289c67
GabrielPw <gxaavier352@gmail.com>
2022-04-11 08:54:56 -0300

    Criação de Busca e Deletar por Id

--- boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
+++ boa-viagem-api/src/main/java/br/com/etechoracio/boa_viagem/controller/GastoController.java
@@ -2,8 +2,11 @@ package br.com.etechoracio.boa_viagem.controller;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,11 +17,27 @@ import br.com.etechoracio.boa_viagem.repository.GastoRepository;
 @RequestMapping("/gastos")
 public class GastoController {
 
+	@Autowired
 	private GastoRepository repository;
 	
+	@GetMapping
 	public List<Gasto> listarTodos(){
 		
 		return repository.findAll();
 	}
 	
+	
+	@GetMapping("/{id}")
+	public Gasto buscarPorId(@PathVariable Long id) {
+		
+		return repository.findById(id).orElse(null);
+	}
+	
+	
+	
+	@DeleteMapping("/{id}")
+	public void deletarPorId(@PathVariable Long id) {
+		
+		repository.deleteById(id);
+	}
 }
