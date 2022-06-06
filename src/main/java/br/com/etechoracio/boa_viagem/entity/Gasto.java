7422c6a49e2934a09e3b3d92f3636db84a15cd10
henquesz <henquesvinicius@gmail.com>
2022-06-06 08:28:00 -0300

    Inclusão das validações com spring

--- src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
+++ src/main/java/br/com/etechoracio/boa_viagem/entity/Gasto.java
@@ -12,6 +12,13 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMax;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.Future;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.*;
 
 import br.com.etechoracio.boa_viagem.enums.CategoriaEnum;
 import lombok.Getter;
@@ -22,7 +29,8 @@ import lombok.Setter;
 @Entity
 @Table(name="TBL_GASTO")
 public class Gasto {
-	
+
+ @NotNull
  @Id
  @Column(name="ID_GASTO")
  @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,6 +39,8 @@ public class Gasto {
  @Column(name="TX_DESCRICAO")
  private String descricao;
  
+ @javax.validation.constraints.NotBlank(message="Campo local é obrigatório.")
+ @Length(max=30)
  @Column(name="TX_LOCAL")
  private String local;
  
@@ -38,13 +48,16 @@ public class Gasto {
  @Column(name="TP_CATEGORIA")
  private CategoriaEnum categoria;
  
+ @NotNull
+ @FutureOrPresent
  @Column(name="DT_GASTO")
  private LocalDate data;
 
+ @DecimalMin(value="0.01")
  @Column(name="VLR_GASTO")
  private Double valor;
  
-
+ @NotNull
  @ManyToOne
  @JoinColumn(name="ID_VIAGEM")
  private Viagem viagem;
--- src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
+++ src/main/java/br/com/etechoracio/boa_viagem/entity/Viagem.java
@@ -10,6 +10,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.DecimalMin;
+import javax.validation.constraints.FutureOrPresent;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
 
 import br.com.etechoracio.boa_viagem.enums.TipoViagemEnum;
 import lombok.Getter;
@@ -21,28 +25,39 @@ import lombok.Setter;
 @Table(name="TBL_VIAGEM")
 
 public class Viagem {
-
+	 
+	 @NotNull
 	 @Id
 	 @Column(name="ID_VIAGEM")
 	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	 private Long id;
 	 
+	 @NotNull
 	 @Column(name="TX_DESTINO")
 	 private String destino;
 	 
+	 @NotNull
 	 @Enumerated(EnumType.STRING)
 	 @Column(name="TP_VIAGEM")
 	 private TipoViagemEnum tipo;
 	 
-	 @Column(name="DT_CHEGADA")
+	 @NotNull
+	 @FutureOrPresent
+	 @Column(name = "DT_CHEGADA")
 	 private LocalDate chegada;
 	 
-	 @Column(name="DT_SAIDA")
+	 @NotNull
+	 @FutureOrPresent
+	 @Column(name = "DT_SAIDA")
 	 private LocalDate saida;
 	 
+	 @NotNull
+	 @DecimalMin(value = "0.01")
 	 @Column(name="VLR_ORCAMENTO")
 	 private Double orcamento;
 	 
+	 @NotNull
+	 @Min(value = 1)
 	 @Column(name="QTD_PESSOAS")
 	 private Integer pessoas;
 	}
